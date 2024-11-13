332ad9048ce30bced4ae95755f5bb79a366d33fc
graywish <naingaunglinn369@gmail.com>
2024-11-14 05:51:19 +0630

    add: php-basic

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo 'Hello World!';
+    ?>
+    <?php
+        $name = 'Yoon Me Me Aung';
+        echo "Hello $name.";
+    ?>
+    <?php
+        $phrase = "To be or not to be";
+        $age = 10;
+        $gpa = 40.54;
+        $isLove = true;
+        null;
+    ?>
+</body>
+</html>
