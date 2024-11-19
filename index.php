4a85a57148d36bd946d910460b851d867f306b02
naingaunglinn <naingaunglinn369@gmail.com>
2024-11-19 12:39:09 +0630

    add: classes & object

--- index.php
+++ index.php
@@ -6,86 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-    include 'header.html'; ?>
-    <!-- <form action="index.php" method="get">
-        Name: <input type="text" name="name">
-        <input type="submit">
-    </form> -->
-    <!-- <form action="index.php" method="post">
-        <input type="checkbox" name="fruits[]" id="" value="oranges">oranges<br/>
-        <input type="checkbox" name="fruits[]" id="" value="bananas">bananas<br/>
-        <input type="checkbox" name="fruits[]" id="" value="apples">apples<br/>
-        <input type="checkbox" name="fruits[]" id="" value="coconuts">coconuts<br/>
-        <input type="submit">
-    </form>
-    <br> -->
-
-    <form action="index.php" method="post">
-        <input type="number" name="num" id="">
-        <input type="submit">
-    </form>
     <?php
-        // $fruits = $_POST['fruits'];
-        // var_dump($fruits);
-    
-        // $isMale = true;
-        // if($isMale){
-        //     echo "He's male";
-        // } else {
-        //     echo "He isn't male";
-        // }
-        
-        // function getMax($num1, $num2, $num3){
-        //     if($num1 >= $num2 && $num1 >= $num3){
-        //         return $num1;
-        //     } elseif($num2 >= $num2 && $num2 >= $num3){
-        //         return $num2;
-        //     } else {
-        //         return $num3;
-        //     }
-        // }
-
-        // echo getMax(3000, 100, 50);
-
-        // $grade = $_POST['grade'];
-
-        // switch($grade){
-        //     case "A+":
-        //         echo "You are great.";
-        //         break;
-        //     case "B":
-        //         echo "You are good.";
-        //         break;
-        //     case "C":
-        //         echo "You are fine.";
-        //         break;
-        //     case "D":
-        //         echo "You need to study.";
-        //         break;
-        //     case "E":
-        //         echo "I'm sorry you didn't make it.";
-        //         break;
-        // }
-
-        // $index = 6;
-        // do{            
-        //     echo $index;
-        //     $index++;
-        // }while($index <= 5)
-        $userInput = $_POST['num'];
-        $luckNumbers = [1,20,35,89,60,2,43,11];
-        for ($i=0; $i < count($luckNumbers); $i++) { 
-            if($userInput == $luckNumbers[$i]){
-                echo "$luckNumbers[$i] is lucky number <br/>";
-            }
+        class Book {
+            public $title;
+            public $author;
+            public $pages;
         }
-        include 'useful-tools.php';
 
-        echo mileInFeet(3);
+        $book1 = new Book;
+        $book1->title = 'Harry Potter';
+        $book1->author = 'J.K Rolling';
+        $book1->pages = 400;
 
-        include 'footer.html';
-    ?>
-    
+        $book2 = new Book;
+        $book2->title = 'Gone with the wind';
+        $book2->author = 'Magret Mechille';
+        $book2->pages = 600;
+
+        echo $book2->pages;
+    ?>    
 </body>
 </html>
